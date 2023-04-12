8be39cb2f9f7fcb520ccebf945ee60d0e6981229
갓댕 <ibbihe@gmail.com>
2023-04-12 22:11:02 +0900

    commit: JPA Practice

---
+++ src/main/java/com/greedy/springjpa/menu/repository/MenuRepository.java
@@ -0,0 +1,65 @@
+package com.greedy.springjpa.menu.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+
+import com.greedy.springjpa.menu.entity.Category;
+import com.greedy.springjpa.menu.entity.Menu;
+
+@Repository
+public class MenuRepository {
+
+	/* 메뉴 조회 */
+	public List<Menu> menuSearch(EntityManager em){
+		
+		String jpql = "SELECT m FROM Menu as m ORDER BY m.menuCode ASC";
+		
+		return em.createQuery(jpql, Menu.class).getResultList();
+	}
+	
+	
+	/* 메뉴 등록 */
+	public void registMenu(EntityManager em, Menu menu) {
+		
+		em.persist(menu);
+	}
+	
+	/* 카테고리 전체 조회 */
+	public List<Category> findCategoryList(EntityManager em){
+		
+		String jpql = "SELECT c FROM Category as c ORDER BY c.categoryCode ASC";
+		
+		return em.createQuery(jpql, Category.class).getResultList();
+	}
+	
+	
+	/* 메뉴 수정 */
+	public void modifyMenu(EntityManager em, Menu menu) {
+		
+		Menu selectMenu = em.find(Menu.class, menu.getMenuCode());
+		
+		selectMenu.setMenuName(menu.getMenuName());
+		selectMenu.setOrderableStatus(menu.getOrderableStatus());
+	}
+
+	/* 메뉴 수정 상세페이지  */
+	public Menu modifyMenuDetail(EntityManager em, int menuCode) {
+
+		return em.find(Menu.class, menuCode);
+	}
+	
+	
+	/* 메뉴 삭제 */
+	public void deleteMenu(EntityManager em, int menuCode) {
+		
+		Menu menu = em.find(Menu.class, menuCode);
+		em.remove(menu);
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/greedy/springjpa/menu/service/MenuService.java
@@ -0,0 +1,76 @@
+package com.greedy.springjpa.menu.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.greedy.springjpa.menu.dto.CategoryDTO;
+import com.greedy.springjpa.menu.dto.MenuDTO;
+import com.greedy.springjpa.menu.entity.Category;
+import com.greedy.springjpa.menu.entity.Menu;
+import com.greedy.springjpa.menu.repository.MenuRepository;
+
+@Service
+public class MenuService {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	private ModelMapper modelMapper;
+	private MenuRepository menuRepository;
+	
+	public MenuService(MenuRepository menuRepository, ModelMapper modelMapper) {
+		this.menuRepository = menuRepository;
+		this.modelMapper = modelMapper;
+	}
+
+	/* 메뉴 조회 */
+	public List<MenuDTO> menuSearch(){
+		
+		List<Menu> menuList = menuRepository.menuSearch(entityManager);
+		return menuList.stream().map(menu -> modelMapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+	}
+	
+	/* 메뉴 등록 */
+	@Transactional
+	public void registMenu(MenuDTO menu) {
+		
+		menuRepository.registMenu(entityManager, modelMapper.map(menu, Menu.class));
+	}
+	
+	/* 카테고리 전체 조회 */
+	public List<CategoryDTO> findCategoryList(){
+		
+		List<Category> cateList = menuRepository.findCategoryList(entityManager);
+		
+		return cateList.stream().map(category -> modelMapper.map(category, CategoryDTO.class)).collect(Collectors.toList());
+	}
+	
+	/***** 메뉴 수정 *****/
+	/* 수정할 메뉴 선택 상세페이지 */
+	public MenuDTO modifyMenuDetail(int menuCode) {
+
+		return modelMapper.map(menuRepository.modifyMenuDetail(entityManager, menuCode), MenuDTO.class); 
+	}
+	
+	/* 메뉴 수정 */
+	@Transactional
+	public void modifyMenu(MenuDTO menu) {
+		
+		menuRepository.modifyMenu(entityManager, modelMapper.map(menu, Menu.class));
+	}
+
+	/***** 메뉴 삭제 *****/
+	@Transactional
+	public void deleteMenu(int menuCode) {
+		
+		menuRepository.deleteMenu(entityManager, menuCode);
+	}
+	
+
+}
